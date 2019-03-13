f0364806c5103275e318ae38b276f2c9dbc12af3
周文童 <zhouwentong@finupgroup.com>
2019-03-13 11:15:43 +0800

    list and tuple

--- zhou/listAndTuple.py
+++ zhou/listAndTuple.py
@@ -1,4 +1,3 @@
-
 # 假设 list 中共有 n 个元素，那可访问的下标为 [0,n-1][-1,-n]，如果超出限制，会报错。
 classmate = ["s1", "s2", "s3"]
 print(classmate[-3])
@@ -17,4 +16,22 @@ print(classmate)
 students = ("a", "b", "c", "d")
 print(students.count("a"))
 
+if 5 < 6 < 7:
+    print("yes")
 
+list1 = [1, 2, 3, 4, 5, 6]
+list2 = [7, 8, 9, 10, 1, 2]
+# python 支持加操作，不支持减操作
+list3 = list1 + list2
+print(list3)
+
+for i in range(4, 10):
+    print(i, end=" ")
+
+s1 = "abcdefg"
+for ch in s1:
+    print(ch, end=" ")
+
+# 元组也可以用这种方法实现
+tuple1 = 1, 2, 3, 4, 65
+print(tuple1)
---
+++ zhou/send_email.py
@@ -0,0 +1,3 @@
+from email.mime.text import MIMEText
+msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+# blocked by company network
